6000ef9534bd80c667c3e5cdadcb008100b7fad6
Paul Wong <20178251+paulwongx@users.noreply.github.com>
2025-07-11 14:02:57 -0400

    fix: Updated index barrel file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export * from "./types";
-export * from "./libs/status-codes";
-export * from "./client";
-export * from "./client-types";
+// Core types
+export type { HttpRouteDefinition } from "./types/http-route-definition";
+export type { HttpStatusCode } from "./types/http-status-code";
+export type { ClientRequestOptions } from "./client-types";
+
+// HTTP constants
+export { HTTP_STATUS_CODE } from "./libs/status-codes";
+
+// Main API functions
+export { httpClient } from "./client";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./http-route-definition";
+export * from "./http-status-code";
 export * from "./schema";
